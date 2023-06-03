bd545a249dad33c64e44bdb662e6b90b78d63a0d
Silenc <fbujtor@yahoo.de>
2023-06-03 21:33:03 +0200

    Moved data for manufactured items to json

---
+++ src/DataJsons/manufacturedItemsModels.ts
@@ -0,0 +1,11 @@
+export interface manufacturableItemCategory {
+  categoryName: string;
+  items: manufacturableItems[];
+}
+
+export interface manufacturableItems {
+  tool: string[]; //TODO: change to tool enum?
+  itemName: string;
+  dc: string;
+  timeInHours: number;
+}
--- src/components/Manufacture.vue
+++ src/components/Manufacture.vue
@@ -19,7 +19,7 @@
           id="itemSelector"
           v-model="selectedItem"
           label="Select Item"
-          :items="manufacturedItemsList"
+          :items="itemsInSelectedCategory"
         />
       </v-col>
     </v-row>
@@ -31,8 +31,17 @@
 <script setup lang="ts">
 import {computed, ref} from "vue";
 
-//TODO move to / calculate from json file
-const itemCategories = ['Armour', 'Simple Weapon', 'Martial Weapon', 'Magitech Firearm', 'Other Item']
+import * as tools from '../DataJsons/tools.json'
+import * as manufacturableItems from '../DataJsons/manufacturedItems.json'
+import {manufacturableItemCategory} from "@/DataJsons/manufacturedItemsModels";
+
+const manufactureCategories: manufacturableItemCategory[] = manufacturableItems.manufactureCategories;
+
+const itemCategories = computed(_ => {
+  return manufactureCategories.map((category) => category.categoryName);
+})
+
+
 const selectedCategory = ref();
 const selectedItem = ref();
 
@@ -40,34 +49,9 @@ function resetItemSelection(): void {
   selectedItem.value = null;
 }
 
-const manufacturedItemsList = computed(_ => {
-  if (selectedCategory.value === 'Armour') {
-    return armourList;
-  }
-  if (selectedCategory.value === 'Simple Weapon') {
-    return ['Simple Weapons', 'Simple Weapons2'];
-  }
-  if (selectedCategory.value === 'Martial Weapon') {
-    return ['Martial Weapons', 'Martial Weapons2'];
-  }
-  if (selectedCategory.value === 'Magitech Firearm') {
-    return ['Magitech Firearms', 'Magitech Firearms2'];
-  }
+const itemsInSelectedCategory = computed(_ => {
+  return manufactureCategories.find(category => category.categoryName === selectedCategory.value)?.items.map(item => item.itemName) ?? [];
 });
-const armourList = [
-  'Padded',
-  'hide',
-  'shield',
-  'Leather',
-  'chain shirt',
-  'ring mail',
-  'Chain mail',
-  'Studded leather',
-  'scale mail',
-  'Breastplate',
-  'splint',
-  'Half plate',
-  'Plate',]
 </script>
 
 <style scoped>

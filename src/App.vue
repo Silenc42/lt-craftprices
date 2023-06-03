3ad394b14b133c31665be4f835db297b24449e89
Silenc <fbujtor@yahoo.de>
2023-06-03 11:43:01 +0200

    initial vuetify setup and minor adjustments

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row align="start" style="height: 150px;">
+          <v-col>
+            <v-img height="150" src="@/assets/logo_loot_tavern.93dd5ad5.webp"/>
+          </v-col>
+          <v-col>
+            <h1>Loot Tavern Crafting Price Calculator</h1> by Silenc42
+          </v-col>
+          <v-col>
+            <v-img height="150" src="@/assets/logo_loot_tavern.93dd5ad5.webp"/>
+          </v-col>
+        </v-row>
+        <v-row>
+          <HelloWorld/>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
